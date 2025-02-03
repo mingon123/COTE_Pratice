8f0c86834ff15fba2c54bedded4c99328c156eac
mingon123 <mingon0125@gmail.com>
2025-02-03 23:19:51 +0900

    프로그래머스 LV.0 코드 처리하기

--- src/src/codingtraining/Solution.java
+++ src/src/codingtraining/Solution.java
@@ -1,7 +1,28 @@
 package src.codingtraining;
 
 class Solution {
-    public int solution(int a, int b, boolean flag) {
-    	return flag == true ? a + b : a - b;
-    }
+	public String solution(String code) {
+		String ret = "";
+		int mode = 0;
+		
+		
+		for (int i = 0; i < code.length(); i++) {
+			char c = code.charAt(i);
+			
+			if (mode == 0) {
+				if ( c != '1' ) { 
+					if (i % 2 == 0) ret += c;
+				} else {
+					mode = 1;
+				}
+			} else {
+				if ( c != '1' ) { 
+					if (i %2 == 1) ret += c;
+				} else {
+					mode = 0;
+				}
+			} 
+		}
+		return ret.isEmpty() ? "EMPTY" : ret;
+	}
 }
