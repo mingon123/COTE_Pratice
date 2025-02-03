824a5ef1b91771ead71a7c4030dca32c5fb5a55e
mingon123 <mingon0125@gmail.com>
2025-02-03 22:30:35 +0900

    프로그래머스 Lv.0 - flag에 따라 다른 값 반환하기

--- src/src/codingtraining/Main.java
+++ src/src/codingtraining/Main.java
@@ -1,4 +1,4 @@
-package codingtraining;
+package src.codingtraining;
 
 public class Main {
 	
---
+++ src/src/codingtraining/Solution.java
@@ -0,0 +1,7 @@
+package src.codingtraining;
+
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+    	return flag == true ? a + b : a - b;
+    }
+}
