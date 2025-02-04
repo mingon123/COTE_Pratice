42716cd39646dad5908c55d0f922729d7f355ad3
SIST <SIST@DESKTOP-8A33JI3>
2025-02-04 12:39:43 +0900

    25/02/04 문제풀이

--- src/src/codingtraining/Solution.java
+++ src/src/codingtraining/Solution.java
@@ -1,8 +1,14 @@
 package src.codingtraining;
 
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] answer = {};
-        return answer;
+    public int[] solution(int[] arr, int[][] queries) {
+    	int[] i = queries[0];
+    	int[] j = queries[1];
+    	
+    	int[] temp = i;
+    	i=j;
+    	j=temp;    	
+
+        return arr;
     }
 }
