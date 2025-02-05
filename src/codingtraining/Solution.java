5256b0e6b496ab738628a8b54359db8bb4c8cf4b
mingon123 <mingon0125@gmail.com>
2025-02-06 02:44:00 +0900

    0206 공부

--- src/codingtraining/Solution.java
+++ src/codingtraining/Solution.java
@@ -1,20 +1,8 @@
 package codingtraining;
 
-import java.util.ArrayList;
-
 class Solution {
-    public int[] solution(int[] arr) {
-    	ArrayList<Integer> stk = new ArrayList<>();
-    	for (int i = 0; i < arr.length; i++) {
-			if(stk.isEmpty()) stk.add(arr[i]);
-			else if(stk.get(stk.size()-1)<arr[i]) stk.add(arr[i]);
-			else if(stk.get(stk.size()-1)>=arr[i]) stk.remove(stk.size()-1); stk.add(arr[i]);
-    	} // for i
-    	
-    	int[]answer = new int[stk.size()]; // ArrayList -> 배열
-    	for (int k = 0; k < stk.size(); k++) {
-			answer[k]=stk.get(k);
-		} // for i		
+    public int solution(String number) {
+        int answer = 0;
         return answer;
     }
 }
