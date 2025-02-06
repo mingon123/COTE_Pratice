1bf79d411320319c1d4cadacceaf4a23b3023eb2
mingon123 <mingon0125@gmail.com>
2025-02-06 12:54:03 +0900

    days0206

--- src/codingtraining/Solution.java
+++ src/codingtraining/Solution.java
@@ -1,8 +1,20 @@
 package codingtraining;
+// 57
 
 class Solution {
-    public int solution(String number) {
-        int answer = 0;
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        int[] answer = {};
+        
+        for (int i = 0; i < intStrs.length; i++) {
+			int intStr = intStrs.toString().charAt(i)-'0';			
+
+			
+			if (intStr>k) answer[i] = Integer.toString(intStr).charAt(i);
+			
+			
+			
+		} // for i
+        
         return answer;
     }
 }
