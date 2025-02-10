ff5002e0fab70fa1d56ce71b2f184e9d0b9d6adf
SIST52 <SIST52@sist>
2025-02-10 18:08:26 +0900

    나는야 돌머리

--- src/programmers/days0210/Lv0_30.java
+++ src/programmers/days0210/Lv0_30.java
@@ -19,6 +19,26 @@ arr	                        queries	                        result
  */
 public class Lv0_30 {
  
-	
+	class Solution {
+		 public int[] solution(int[] arr, int[][] queries) {
+				int[] answer = {};
+				
+				
+				for(int i = 0; i < queries.length; i++) {
+					int s = queries[i][0];
+					int e = queries[i][1];
+					int k = queries[i][2];
+					for(int j = s; j <= e; j++)
+					if( (j % k) == 0 ) {
+						arr[j] += 1;
+					}
+				}
+				
+				return answer = arr;
+
+
+		 }
+		  
+		}
 	
 }
