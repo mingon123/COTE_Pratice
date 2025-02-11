46de969cb048580fa307ffbafb4e1ae5534079c8
user <user@DESKTOP-S6UUK9H>
2025-02-11 12:32:06 +0900

    백준 1931 5430

--- src/codingtraining/Baekjoon5430.java
+++ src/codingtraining/Baekjoon5430.java
@@ -1,61 +1,79 @@
 package codingtraining;
 
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Baekjoon5430 {
 
+	public static void main(String[] args) throws NumberFormatException, IOException{
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		List<Node> list = new ArrayList<>();
-
-		StringTokenizer st;
-		while(N-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int dest = Integer.parseInt(st.nextToken());
-			list.add(new Node(start,dest));
-		}
-
-		Collections.sort(list);
-
-		int pos = 0;
-		int count = 0;
-
-		Iterator<Node> it = list.iterator();
-		while(it.hasNext()) {
-			Node now = it.next();
-			if(pos <= now.start) {
-				pos = now.dest;
-				count++;
-			}else {
-				continue;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int test = Integer.parseInt(br.readLine());
+		Loop : while(test-- > 0) {
+			char[] chArr = br.readLine().toCharArray();
+			int numCount = Integer.parseInt(br.readLine());
+			int[] intArr = new int[numCount];
+			StringBuilder sb = new StringBuilder(br.readLine());
+			sb.deleteCharAt(sb.length()-1);
+			sb.deleteCharAt(0); // [] 삭제 숫자와 ,로만 이뤄진 무나졍ㄹ
+			StringTokenizer st = new StringTokenizer(sb.toString(), ",");
+			for(int i = 0; i < numCount ; i ++) {
+				intArr[i] = Integer.parseInt(st.nextToken());
 			}
-		}
-		System.out.println(count);
-	}
-}
 
-class Node implements Comparable<Node>{
-	int start;
-	int dest;
-	@Override
-	public int compareTo(Node o) {
-		if(this.dest == o.dest) return this.start - o.start;  
-		return this.dest - o.dest; // 시작점 ~ 다음 출발점 가장 적은경우부터 정렬
-	}
-	public Node(int start, int dest) {
-		super();
-		this.start = start;
-		this.dest = dest;
+			List<Integer> list= new ArrayList<>();
+
+			for(int num : intArr) {
+				list.add(num);
+			}
+
+			boolean reverse = false;
+
+			for(int i = 0 ; i < chArr.length ; i++) {
+				if(chArr[i] == 'D') {
+					if(list.isEmpty()) {
+						bw.write("error\n");
+						continue Loop;
+					}
+
+					if(reverse) {
+						list.remove(list.size()-1);
+					}else {
+						list.remove(0);
+					}
+
+				}else {
+					reverse = !reverse;
+				}
+			}
+			if(reverse) {
+				Collections.reverse(list);
+			}
+			sb = new StringBuilder();
+			sb.append("[");
+			if(list.size() != 0) {
+				for(int num : list) {
+					sb.append(num).append(",");
+				}
+				sb.deleteCharAt(sb.length()-1);
+			}
+			sb.append("]");
+			bw.write(sb.toString());
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
