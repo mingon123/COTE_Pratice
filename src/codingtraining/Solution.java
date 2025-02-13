46efdc7ff3bfd266a7d225c828a6e4c0e00fb5bd
mingon123 <mingon0125@gmail.com>
2025-02-14 06:34:42 +0900

    d0214

--- src/codingtraining/Solution.java
+++ src/codingtraining/Solution.java
@@ -1,18 +1,20 @@
 package codingtraining;
 
 class Solution {
-    public int solution(String[] s1, String[] s2) {
-        int answer = 0;
-        return answer;
+    public int solution(String my_string) {
+    	String [] parts = my_string.split(" ");
+    	int num = Integer.parseInt(parts[0]);
+    	
+    	return 0;
     }
 }
 
-//문제 요약 : num을 이루는 숫자 중 k가 있으면 num의 그 숫자가 있는 자리수 반환 없으면 -1 반환
+//문제 요약 : my_string은 3+5형태. +,- 연산자만 존재,공백X. 수식 계산한 값 출력
 
 
 
 
 
-// 150
+// 157
 
 
