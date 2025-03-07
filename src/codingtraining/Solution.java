71d361629c5e1ae0b22ea192527bf81a1737ca7a
mingon123 <mingon0125@gmail.com>
2025-03-07 13:38:30 +0900

    d0307

--- src/codingtraining/Solution.java
+++ src/codingtraining/Solution.java
@@ -3,23 +3,55 @@ package codingtraining;
 import java.util.*;
 
 class Solution {
-    public String solution(int[] numbers, String hand) {
-    	String answer = "";
-    	String current = answer.substring(answer.length()-1);
-    	for (int i = 0; i < numbers.length; i++) {
-			if(numbers[i]==1 || numbers[i]==4 || numbers[i]==7) answer += "L";
-			else if(numbers[i]==3 || numbers[i]==6 || numbers[i]==9) answer += "R";
-			else {
-				if(current == "L") answer+="L";
-				else if(current =="R") answer+="R";				
+	public String solution(int[] numbers, String hand) {
+		StringBuffer answer = new StringBuffer();
+		int[] Lnumber = {3,0}, Rnumber = {3,2};
+		for (int i = 0; i < numbers.length; i++) {
+			if(numbers[i]%3==1) {
+				answer.append("L");
+				Lnumber = getNumber(numbers[i]);
 			}
-    		
-    		
-    		
-    		
+			else if(numbers[i]%3==0 && numbers[i]!=0) {
+				answer.append("R");
+				Rnumber = getNumber(numbers[i]);
+			}
+
+			else {
+				int[] number = getNumber(numbers[i]);
+				int lNum = Math.abs(Lnumber[0]-number[0]) + Math.abs(Lnumber[1]-number[1]);
+				int rNum = Math.abs(Rnumber[0]-number[0]) + Math.abs(Rnumber[1]-number[1]);
+
+				if(lNum == rNum) {
+					if(hand.equals("right")){
+						answer.append("R");
+						Rnumber = getNumber(numbers[i]);
+					} else {
+						answer.append("L");
+						Lnumber = getNumber(numbers[i]);
+					}
+				}
+				else if(lNum < rNum) {
+					answer.append("L");
+					Lnumber = getNumber(numbers[i]);
+				}
+				else {
+					answer.append("R");
+					Rnumber = getNumber(numbers[i]);
+				}
+			}
 		} // for i
-        return answer;
-    }
+		return new String(answer);
+	}
+
+	private int[] getNumber(int num) {
+		int[][] keypad = {{1,2,3},{4,5,6},{7,8,9},{-2,0,-3}};
+		for (int i = 0; i < keypad.length; i++) {
+			for (int j = 0; j < keypad[i].length; j++) {
+				if(keypad[i][j] == num) return new int[]{i,j};				
+			} // for j
+		} // for i
+		return new int[] {-1,-1};
+	}
 }
 
 
@@ -31,7 +63,6 @@ class Solution {
 // return : 누른 손가락 순서 L<->R
 
 
-// 46 - 키패드
 
 // 5 - lv1
 // 170 - lv0
